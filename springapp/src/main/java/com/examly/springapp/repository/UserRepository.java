f27bb403c088cca8112c659ba8a0a4e2ea2ac158
chandu20032000 <97679868+chandu20032000@users.noreply.github.com>
2022-03-17 10:30:04 +0530

    second

--- springapp/src/main/java/com/examly/springapp/repository/UserRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 @Repository
-public interface UserRepository extends JpaRepository<UserModel,Long> {
+public interface UserRepository extends JpaRepository<UserModel,Long>{
     @Query("SELECT u FROM UserModel u Where u.email = ?1") 
     UserModel findByEmail(String email);
     @Query("SELECT u FROM UserModel u Where u.email = ?1 and u.password = ?2")
